44e14564f7ef683732883575755aa9b1bea507dc
Joerg Beekmann <jbeeko>
2017-01-31 14:06:16 -0800

    check adding for overflow

--- src/money/money.fs
+++ src/money/money.fs
@@ -19,6 +19,8 @@ type Currency = {
     MajorUnit : string
     MinorUnit : string
 }
+
+let maxValue = Decimal.MaxValue/1000000000000m
 let CAD = {Code = CAD; Number = 444; Name = "Canadian Dollar"; Decimals = 2; MajorUnit = "Dollar"; MinorUnit = "cent"}
 let USD = {Code = USD; Number = 444; Name = "US Dollar"; Decimals = 2; MajorUnit = "Dollar"; MinorUnit = "cent"}
 let GBP = {Code = GBP; Number = 444; Name = "Pound Stirling"; Decimals = 2; MajorUnit = "Pound"; MinorUnit = "pence"}
@@ -30,8 +32,8 @@ type Money =
     with
         static member Parse(str) =
             Value(33m, CAD)
-        override self.ToString() =
-            match self with
+        override this.ToString() =
+            match this with
             | Value (amt, cur) ->
                 let formatStr = 
                     if amt = Decimal.Round(amt, cur.Decimals)
@@ -40,12 +42,14 @@ type Money =
                 let twFormat = Printf.StringFormat<decimal->CurrencyCode->string>(formatStr)
                 sprintf twFormat amt cur.Code
             | NaV err -> sprintf "NaV %A" err
-        member self.AsString = 
-            self.ToString() 
+        member this.AsString = 
+            this.ToString() 
+        member this.AddValue m =
+            Value(33m, CAD)
 
 
-let addValues v1 v2 = 
-    match (v1, v2) with
+let addValues m1 m2 = 
+    match (m1, m2) with
     | (NaV err, _) | (_, NaV err) -> NaV err
     | (Value(amt1, cur1), Value(amt2, cur2)) when cur1 = cur2 ->
         try Value (amt1 + amt2, cur1)
@@ -58,21 +62,26 @@ let (.+.) = addValues
 let multiplyBy money factor =
     match money with
     | Value (amt, cur) -> 
-        try Value (amt*factor, cur)
+        try 
+            let tmp = amt*factor
+            if tmp > maxValue
+            then NaV Overflow
+            else Value (tmp, cur)
         with 
             | :? OverflowException as ex -> NaV Overflow
-    | NaV err -> NaV err
+    | NaV err -> money
 
-let devideBy money divisor  =
+let divideBy money divisor  =
     match money with
     | Value (amt, cur) -> 
         if divisor  = 0m
         then NaV DivBy0
         else 
-            try Value (amt/divisor , cur)
+            try 
+                Value (amt/divisor , cur) 
             with 
                 | :? OverflowException as ex -> NaV Overflow
-    | NaV err -> NaV err
+    | NaV err -> money
 
 let (.*.) = multiplyBy
 
@@ -85,40 +94,4 @@ let negate money =
     | NaV err -> NaV err
 
 
-// open Currencies
-
-// type Payment = {
-//     Value : Money;
-//     Comment : String;
-// }
-// type DBRec = {
-//     CurrencyCode : String;
-//     Amount : decimal;
-//     Comment : String
-// }
-// let data = 
-//     [
-//         {CurrencyCode = "USD"; Amount = 5.50m; Comment = "Latte"}
-//         {CurrencyCode = "USD"; Amount = 6.50m; Comment = "Scone"}
-//         {CurrencyCode = "CAD"; Amount = 4.33m; Comment = "Americano"}
-//     ]
-
-// let makePymt (dbRec : DBRec) = 
-//     match dbRec.CurrencyCode with
-//     | "USD" -> {Value = Value(dbRec.Amount, USD); Comment = dbRec.Comment}
-//     | "CAD" -> {Value = Value(dbRec.Amount, CAD); Comment = dbRec.Comment}
-//     | "GBP" -> {Value = Value(dbRec.Amount, GBP); Comment = dbRec.Comment}
-//     | _ -> failwith "bad currency"
-
-
-// let pymts =
-//     data 
-//         |> List.map makePymt
-
-// pymts
-//     |> List.map (fun p -> p.Value)
-//     |> List.reduce addValues
-
-
-// CAD
 
--- test/Money.UnitTests/Tests.fs
+++ test/Money.UnitTests/Tests.fs
@@ -1,15 +1,48 @@
 module Tests
 
 open Expecto
+open Money
+open System
+
+
+type Payment = {
+    Value : Money;
+    Comment : string;
+}
+type DBRec = {
+    CurrencyCode : string;
+    Amount : decimal;
+    Comment : string
+}
+let data = 
+    [
+        {CurrencyCode = "USD"; Amount = 5.50m; Comment = "Latte"}
+        {CurrencyCode = "USD"; Amount = 6.50m; Comment = "Scone"}
+        {CurrencyCode = "CAD"; Amount = 4.33m; Comment = "Americano"}
+    ]
+
+let makePymt (dbRec : DBRec) = 
+    match dbRec.CurrencyCode with
+    | "USD" -> {Value = Value(dbRec.Amount, USD); Comment = dbRec.Comment}
+    | "CAD" -> {Value = Value(dbRec.Amount, CAD); Comment = dbRec.Comment}
+    | "GBP" -> {Value = Value(dbRec.Amount, GBP); Comment = dbRec.Comment}
+    | _ -> failwith "bad currency"
+
+
+let pymts =
+    data 
+        |> List.map makePymt
+
+let v1 = Value(33m, CAD)
+let v2 = Value(10m, USD)
+let errMixed = NaV MixedCurr
+let errDiv0 = NaV DivBy0
+let errOver = NaV Overflow
 
 [<Tests>]
 let tests =
-  testList "samples" [
-    testCase "universe exists" <| fun _ ->
-      let subject = true
-      Expect.isTrue subject "I compute, therefore I am."
-
-    testCase "should fail" <| fun _ ->
-      let subject = false
-      Expect.isTrue subject "I should fail because the subject is false."
+  testList "UnitTests" [
+    testCase "mixedCur" <| fun _ -> Expect.equal  (addValues v1 v2)  errMixed ""
+    testCase "div0" <| fun _ -> Expect.equal  (divideBy v1 0m)  errDiv0 ""
+    testCase "overflow" <| fun _ -> Expect.equal  (multiplyBy v1 Decimal.MaxValue)  errOver ""
   ]
